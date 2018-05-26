54d7d03598a5ebbfafe4e69aab3a9eebd742bc05
DESKTOP-HEREPGC\Tudor Filp <tudorf90@hotmail.com>
2018-05-26 13:46:23 +0300

    facebook login integrated successfully. Testing..

--- app/src/main/java/com/racoders/racodersproject/MainActivity.java
+++ app/src/main/java/com/racoders/racodersproject/MainActivity.java
@@ -1,15 +1,91 @@
 package com.racoders.racodersproject;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+public class MainActivity extends AppCompatActivity {
+    public LoginButton loginButton;
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createKeyHash();
+        setTitle("Login");
+        loginButton = findViewById(R.id.fb_login_id);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i("result", "Successful" + loginResult.getAccessToken().getUserId()
+                + " " + loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+
+                Log.i("result", "Cancelled");
+
+            }
+
+            @Override
+            public void onError(FacebookException error){
+                Log.i("result", "Failed" + error.toString());
+            }
+        });
+
         Toast.makeText(this, "Welcome to our test app", Toast.LENGTH_SHORT).show();
+
+
+        //https://discovercluj-5f88f.firebaseapp.com/__/auth/handler
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+    public void createKeyHash(){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.racoders.racodersproject",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
     }
 }
