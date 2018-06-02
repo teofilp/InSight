eb4f8a52c1ba608d5ef20eb4233b1436ea1d85b0
DESKTOP-HEREPGC\Tudor Filp <tudorf90@hotmail.com>
2018-06-02 22:16:49 +0300

    simple login created

--- app/src/main/java/com/racoders/racodersproject/MainActivity.java
+++ app/src/main/java/com/racoders/racodersproject/MainActivity.java
@@ -1,15 +1,57 @@
 package com.racoders.racodersproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button LogIn;
+    private int counter=5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Welcome to our test app", Toast.LENGTH_SHORT).show();
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        LogIn = (Button)findViewById(R.id.btnLogin);
+
+        Info.setText("No of attempts remaining: 5");
+
+        LogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+    }
+
+
+    private void validate (String userName, String userPassword){
+        if((userName.equals("Admin")) && (userPassword.equals("1234"))){
+            Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+            startActivity(intent);
+        }
+        else{
+            counter--;
+
+            Info.setText("No of attempts remaining: "+ String.valueOf(counter));
+
+            if(counter == 0){
+                LogIn.setEnabled(false);
+            }
+        }
     }
 }
