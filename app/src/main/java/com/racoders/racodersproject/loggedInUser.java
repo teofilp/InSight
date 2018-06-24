5af251113a8519b4fd0a69b65e18c9da38eb4d35
DESKTOP-HEREPGC\Tudor Filp <tudorf90@hotmail.com>
2018-06-24 20:21:49 +0300

    register and sign in with local account put together.. all working fine , also added cameraView when logged in :)

--- app/src/main/java/com/racoders/racodersproject/loggedInUser.java
+++ app/src/main/java/com/racoders/racodersproject/loggedInUser.java
@@ -1,15 +1,21 @@
 package com.racoders.racodersproject;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -34,13 +40,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class loggedInUser extends AppCompatActivity {
+public class loggedInUser extends AppCompatActivity implements SurfaceHolder.Callback{
 
+    Camera camera;
+
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    final private static int CAMERA_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in_user);
+
+        surfaceView = findViewById(R.id.surfaceView);
+
+        surfaceHolder = surfaceView.getHolder();
+
+        if(ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        }else{
+            surfaceHolder.addCallback(this);
+            surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
 
     }
     public void signOut(View view){
@@ -48,4 +70,46 @@ public class loggedInUser extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(), MainActivity.class));
     }
 
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        camera = Camera.open();
+
+        Camera.Parameters parameters;
+        parameters = camera.getParameters();
+
+        camera.setDisplayOrientation(90);
+        parameters.setPreviewFrameRate(60);
+        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+
+        try{
+            camera.setPreviewDisplay(holder);
+        }catch (IOException e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        camera.startPreview();
+
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if(requestCode == CAMERA_REQUEST_CODE){
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    surfaceHolder.addCallback(this);
+                    surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+                }else{
+                    Toast.makeText(this, "Please provide the permission", Toast.LENGTH_SHORT).show();
+                }
+            }
+    }
 }
